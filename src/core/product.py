39bdc20d2161788fa2f907508b4f79a5fdad7dea
AlexIrnazarov <aleksandr.irnazarov@mail.ru>
2025-04-29 14:19:58 +0500

    Add new image linked attributes to product class

--- src/core/product.py
+++ src/core/product.py
@@ -1,6 +1,8 @@
 import requests
 from .sort_type import SortType
 from .wb_search import search_by_id, search_by_query
+from core.get_basket_id import get_basket_id
+from core.image import convert_to_png, scrap_image
 
 class Product:
 
@@ -12,6 +14,13 @@ class Product:
         self.__price = self.parse_price() 
         self.__review_rating = self.__data['reviewRating']
         self.__supplier_rating = self.__data['supplierRating']
+        self.__image_link = ( 
+            f"https://basket-{get_basket_id(product_id)}.wbbasket.ru/vol{product_id // 10**5}/"
+            f"part{product_id // 10**3}/{product_id}/images/big/1.webp" 
+        )
+        self.__image_folder_path = f".\\images\\{product_id}"
+        self.__temporary_path = scrap_image(self.__image_link, self.__image_folder_path)
+        self.__image_path = convert_to_png(self.__temporary_path, self.__image_folder_path)
 
     @property
     def data(self):
@@ -40,6 +49,18 @@ class Product:
     @property
     def supplier_rating(self):
         return self.__supplier_rating
+
+    @property
+    def image_link(self):
+        return self.__image_link
+
+    @property
+    def folder_path(self):
+        return self.__image_folder_path
+
+    @property
+    def image_path(self):
+        return self.__image_path
 
     # make examples printable
     def __str__(self):
