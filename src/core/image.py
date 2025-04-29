4467c9f236ab50527a55b725e31d2c91680f4030
AlexIrnazarov <aleksandr.irnazarov@mail.ru>
2025-04-29 14:21:25 +0500

    New methods for image scrapping

---
+++ src/core/image.py
@@ -0,0 +1,32 @@
+﻿import requests
+import re
+import os
+from PIL import Image
+from core.get_basket_id import get_basket_id
+
+def scrap_image(image_link: str, image_folder_path: str):
+    response = requests.get(image_link, stream=True)
+    image_path = f"{image_folder_path}\\image.webp"
+    os.makedirs(image_folder_path)
+
+    if response.status_code == 200:
+        # make and open an image file
+        with open(image_path, "xb") as file:
+            # write info chunkes down in the file
+            for chunk in response.iter_content(1024):
+                file.write(chunk) 
+        print(f"The image was successfully saved as {image_path}")
+        return image_path
+    else:
+        print(f"ERROR: {response.status_code}")
+
+def convert_to_png(image_path: str, image_folder_path: str):
+    try:
+        image = Image.open(image_path)
+        new_image_path = f"{image_folder_path}\\image.png"
+        image.save(new_image_path)
+        os.remove(image_path)
+        print(f"The image was successfully converted and saved as {new_image_path}")
+        return new_image_path
+    except Exception as exception:
+        print(f"Convertation Error: {exception}")
--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@
 from core.wb_search import search_by_id, search_by_query
 from core.products import Products
 from core.product import Product
+from core.image import scrap_image, convert_to_png
 import requests
 
-products = Products('белая куртка', SortType.POPULAR.value)
+products = Products('синяя куртка', SortType.POPULAR.value)
 
 for product_id in products.yiled_id():
     product = Product(product_id)
