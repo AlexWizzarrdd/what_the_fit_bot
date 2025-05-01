9062d803d4de109a36329a636a7faac41acf9b56
AlexIrnazarov <aleksandr.irnazarov@mail.ru>
2025-05-01 21:52:01 +0500

    Make key method that do all needed stuff

---
+++ src/core/wb_parse.py
@@ -0,0 +1,39 @@
+from core.product import Product
+from core.products import Products
+from core.sort_type import SortType
+
+
+def parse_products_data(query: str, sort_type: SortType = SortType.POPULAR.value, product_amount: int = 3):
+    products = Products(query, sort_type)
+    
+    """
+    Return wildberries products data from query printing process.
+
+    Args:
+    query (string): search query.
+    sort_type (SortType): sort type (SortType.POPULAR.value is by default).
+    product_amount (int): output amount of products.
+    
+    Returns:
+    dict: product data dictionaries
+    
+    Example:
+    >>> wb_parse("blue jeans", SortType.Rate.value, 5)
+    {0: {_Product__brand: brand_name, ...}, 
+    1: {...}, 
+    4: {...}
+    }
+    """
+
+    products_data = {}
+
+    for i in range(product_amount):
+        product_id = products.get_id(i)
+        product = Product(product_id)
+        current_attributes = product.__dict__
+        for ignored_attribute in product.ignored_attributes:
+            del current_attributes[ignored_attribute] # delete attributes that we don't want user to see
+        products_data[i] = current_attributes
+        print(product)
+
+    return products_data
