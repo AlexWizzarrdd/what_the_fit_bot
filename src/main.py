e68508caa6e972ae060c38fd1a096769cbc22219
AlexIrnazarov <aleksandr.irnazarov@mail.ru>
2025-05-01 21:53:20 +0500

    use gusto method!

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
 ﻿from core.sort_type import SortType
+from core.wb_parse import parse_products_data
 from core.wb_search import search_by_id, search_by_query
 from core.products import Products
 from core.product import Product
 from core.image import scrap_image, convert_to_png
 import requests
 
-products = Products('синяя куртка', SortType.POPULAR.value)
-
-for product_id in products.yiled_id():
-    product = Product(product_id)
-    print(product)
+products_data = parse_products_data('красная шляпа')
