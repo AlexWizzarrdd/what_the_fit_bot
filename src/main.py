b10a79adfdcb9aaf5cd1c85d9eaf19ab6c87919a
AlexIrnazarov <aleksandr.irnazarov@mail.ru>
2025-06-15 18:00:55 +0500

    new parser work example in main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
-﻿from core.parser.wb_parse import wb_parse
+﻿from core.parser.product import SortType
+from core.parser.wb_parse import wb_parse
+from core.parser.criteria import Criteria
 
-products_data = wb_parse('white shirt')
+products_data = wb_parse('синяя футболка', SortType.PRICEDOWN.value, 5, 
+                         True, Criteria.has_feedbacks)
