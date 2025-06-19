4d11bfdb7766fdd17fd3fc973f0dcffd00fce92e
AlexIrnazarov <aleksandr.irnazarov@mail.ru>
2025-06-19 13:13:44 +0500

    Images are being saved in temp folder

--- src/core/parser/image.py
+++ src/core/parser/image.py
@@ -5,13 +5,12 @@ import os
 from PIL import Image
 from core.parser.get_basket_id import get_basket_id
 import time
+import tempfile
 
-def scrap_image(image_link: str, image_folder_path: str):
+def scrap_image(image_link: str):
     response = requests.get(image_link, stream=True)
-    image_path = get_new_image_path(image_folder_path, 'webp')
-
-    if not os.path.isdir(image_folder_path):
-        os.makedirs(image_folder_path)
+    temp_folder = tempfile.gettempdir()
+    image_path = get_new_image_path(temp_folder, 'webp')
 
     if response.status_code == 200:
         with open(image_path, "xb") as file:
@@ -23,10 +22,10 @@ def scrap_image(image_link: str, image_folder_path: str):
     else:
         print(f"ERROR: {response.status_code}")
 
-def convert_to_png(image_path: str, image_folder_path: str):
+def convert_to_png(image_path: str):
     try:
         image = Image.open(image_path)
-        new_image_path = get_new_image_path(image_folder_path, 'png')
+        new_image_path = re.sub("webp", "png", image_path)
         image.save(new_image_path)
         os.remove(image_path)
         print(f"The image was successfully converted and saved as {new_image_path}")
@@ -34,12 +33,19 @@ def convert_to_png(image_path: str, image_folder_path: str):
     except Exception as exception:
         print(f"Convertation Error: {exception}")
 
-def save_image(image_link: str, image_folder_path: str):
-    image_path = scrap_image(image_link, image_folder_path)
-    new_image_path = convert_to_png(image_path, image_folder_path)
-    return new_image_path
-
+def save_image(image_link: str):
+    try:
+        image_path = scrap_image(image_link)
+        new_image_path = convert_to_png(image_path)
+        return new_image_path
+    except Exception as exception:
+        print(f"Can't save image': {exception}")
+    
 def get_new_image_path(image_folder_path: str, file_extension: str):
+    if not os.path.isdir(image_folder_path):
+        raise ValueError("Wrong folder path!")
+    if not re.match(r"[a-z]+", file_extension):
+        raise ValueError("Wrong folder path!")
     image_path = f"{image_folder_path}\\image.{file_extension}"
     time_stamp = time.strftime("%Y%m%d_%H%M%S")
     core, extension = os.path.splitext(image_path)
